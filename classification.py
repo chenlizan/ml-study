9e1ec29b7623f4e0a77bdf27678f0c26a4e1f212
chenlizan <clz_318@live.com>
2024-05-22 17:53:15 +0800

    5.22

--- classification.py
+++ classification.py
@@ -20,7 +20,41 @@ for i in range(25):
     plt.subplot(5, 5, i + 1)
     plt.xticks([])
     plt.yticks([])
-    plt.grid(False)
-    plt.imshow(x_train[i], cmap=plt.cm.binary)
-    plt.xlabel([y_train[i]])
+    plt.imshow(x_test[i], cmap=plt.cm.binary)
+    plt.xlabel(y_test[i])
 plt.show()
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.Dense(10)
+])
+
+model.summary()
+
+'''
+优化器 - 决定模型如何根据其看到的数据和自身的损失函数进行更新。
+损失函数 - 测量模型在训练期间的准确程度。你希望最小化此函数，以便将模型“引导”到正确的方向上。
+指标 - 用于监控训练和测试步骤。以下示例使用了准确率，即被正确分类的图像的比率。
+'''
+
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test, y_test, verbose=2)
+
+model.save('.keras/my_model.keras')
+
+new_model = tf.keras.models.load_model('.keras/my_model.keras')
+
+new_model.summary()
+
+probability_model = tf.keras.Sequential([
+    new_model,
+    tf.keras.layers.Softmax()
+])
+
+print(probability_model(x_test[:10]))
---
+++ perceptron.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import ListedColormap
+from sklearn import datasets
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=cl,
+                    edgecolor='black'
+                    )
+
+
+iris = datasets.load_iris()
+df = iris.data
+
+y = iris.target[:100]
+X = iris.data[:100, :2]
+
+# plot data
+plt.scatter(X[:50, 0], X[:50, 1],
+            color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color='blue', marker='x', label='versicolor')
+
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+
+plt.show()
+
+# ppn = Perceptron(eta0=0.1, n_iter_no_change=10, random_state=1)
+ppn = SGDClassifier(loss="perceptron", eta0=0.1, learning_rate="constant", n_iter_no_change=10, penalty=None,
+                    random_state=1)
+
+ppn.fit(X, y)
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+
+plt.show()
