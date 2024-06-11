71ec3ed9deba582db7f6259bb88199f2d5dbefb1
chenlizan <clz_318@live.com>
2024-06-11 18:19:45 +0800

    6.11

--- chapter14.py
+++ chapter14.py
@@ -1,4 +1,6 @@
 import datetime
+
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
@@ -82,4 +84,90 @@ with g.as_default():
 
 with tf.compat.v1.Session(graph=g) as sess:
     sess.run(init_op)
-    print(sess.run(w2))
+    # print(sess.run(w2))
+
+with g.as_default():
+    with tf.compat.v1.variable_scope("net_A"):
+        with tf.compat.v1.variable_scope("layer-1"):
+            w1 = tf.Variable(tf.compat.v1.random_normal(shape=(10, 4)), name="weights")
+        with tf.compat.v1.variable_scope("layer-2"):
+            w2 = tf.Variable(tf.compat.v1.random_normal(shape=(20, 10)), name="weights")
+    with tf.compat.v1.variable_scope("net_B"):
+        with tf.compat.v1.variable_scope("layer-1"):
+            w3 = tf.Variable(tf.compat.v1.random_normal(shape=(10, 4)), name="weights")
+print(w1)
+print(w2)
+print(w3)
+
+
+def build_classifier(data, labels, num_classes=2):
+    data_shape = data.get_shape().as_list()
+    weights = tf.compat.v1.get_variable(name="weights", shape=[data_shape[1], num_classes], dtype=tf.float32)
+    bias = tf.compat.v1.get_variable(name="bias", initializer=tf.zeros(shape=num_classes))
+    logits = tf.add(tf.matmul(data, weights), bias, name="logits")
+    print(logits)
+    return logits, tf.nn.softmax(logits)
+
+
+def build_generator(data, n_hidden):
+    data_shape = data.get_shape().as_list()
+    w1 = tf.Variable(tf.compat.v1.random_normal(shape=(data_shape[1], n_hidden)), name="w1")
+    b1 = tf.Variable(tf.compat.v1.zeros(shape=n_hidden), name="b1")
+    hidden = tf.add(tf.matmul(data, w1), b1, name="hidden_pre-activation")
+    hidden = tf.nn.relu(hidden, "hidden_activation")
+    w2 = tf.Variable(tf.compat.v1.random_normal(shape=(n_hidden, data_shape[1])), name="w2")
+    b2 = tf.Variable(tf.compat.v1.zeros(shape=data_shape[1]), name="b2")
+    output = tf.add(tf.matmul(hidden, w2), b2, name="output")
+    return output, tf.nn.sigmoid(output)
+
+
+batch_size = 64
+
+with g.as_default():
+    tf_X = tf.compat.v1.placeholder(shape=[batch_size, 2], dtype=tf.float32, name='tf_X')
+
+    with tf.compat.v1.variable_scope("generator"):
+        gen_out1 = build_generator(data=tf_X, n_hidden=50)
+
+    with tf.compat.v1.variable_scope("classifier") as scope:
+        cls_out1 = build_classifier(data=tf_X, labels=tf.ones(shape=batch_size))
+        scope.reuse_variables()
+        cls_out2 = build_classifier(data=cls_out1[1], labels=tf.zeros(shape=batch_size))
+        print(cls_out2)
+
+        init_op = tf.compat.v1.global_variables_initializer()
+
+with g.as_default():
+    tf.compat.v1.set_random_seed(123)
+
+    tf_x = tf.compat.v1.placeholder(shape=(None), dtype=tf.float32, name='tf_x')
+    tf_y = tf.compat.v1.placeholder(shape=(None), dtype=tf.float32, name='tf_y')
+    weight = tf.Variable(tf.compat.v1.random_normal(shape=(1, 1), stddev=0.25), name="weight")
+    bias = tf.Variable(0.0, name="bias")
+    y_hat = tf.add(weight * tf_x, bias, name="y_hat")
+    cost = tf.reduce_mean(tf.square(tf_y - y_hat), name="cost")
+    optim = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.001)
+    train_op = optim.minimize(cost, name="train_op")
+
+
+def make_random_data():
+    x = np.random.uniform(low=2, high=4, size=100)
+    y = []
+    for t in x:
+        r = np.random.normal(loc=0.0, scale=(0.5 + t * t / 3), size=None)
+        y.append(r)
+    return x, 1.726 * x - 0.84 + np.array(y)
+
+
+x, y = make_random_data()
+
+plt.plot(x, y, 'o')
+plt.show()
+
+x_train, y_train = x[:100], y[:100]
+x_test, y_test = x[100:], y[100:]
+
+n_epochs = 500
+training_costs = []
+with tf.compat.v1.Session(graph=g) as sess:
+    sess.run(tf.compat.v1.global_variables_initializer())
